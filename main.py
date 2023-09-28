a6bbb481f6f2296e6fdef5e91546280cba52a065
nithindaniel1 <nithindaniel2018@gmail.com>
2023-09-28 21:30:06 +0530

    made some changes in code and added setcommand line

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 arr = ['1','2']
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     context_value = context.args
-    if  len(context_value) < 0:
+    if not len(context_value) <= 0:
         if not context_value in arr:
             await context.bot.send_message(
                 chat_id=update.effective_chat.id,
@@ -23,9 +23,9 @@ if __name__ == '__main__':
     application = ApplicationBuilder().token(config('TOKEN')).build()
     
     start_handler = CommandHandler('start', start)
-    caps_handler = CommandHandler('caps', caps)
+    # caps_handler = CommandHandler('caps', caps)
 
     application.add_handler(start_handler)
-    application.add_handler(caps_handler)
+    # application.add_handler(caps_handler)
 
     application.run_polling()
